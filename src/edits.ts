9895b6217199f9d2598b95cc2678fc5292511b15
tony <tony@tillpos.com.au>
2021-11-09 11:17:42 +0700

    Remove unused log

--- src/edits.ts
+++ src/edits.ts
@@ -83,8 +83,6 @@ export async function uploadToPlayStore(options: EditOptions, releaseFiles: stri
 
         // Commit the pending Edit
         core.info(`Committing the Edit`)
-        core.info(`With applicationId: ${options.applicationId}`)
-        core.info(`With changesNotSentForReview: ${options.changesNotSentForReview}`)
         const res = await androidPublisher.edits.commit({
             auth: options.auth,
             editId: appEdit.data.id!,
